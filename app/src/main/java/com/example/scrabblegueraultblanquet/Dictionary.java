327efb029aa2d2f00141224fe4213dad77722954
Yanis Guérault <yanis.guerault@orange.com>
2019-12-12 16:33:37 +0100

    I start

---
+++ app/src/main/java/com/example/scrabblegueraultblanquet/Dictionary.java
@@ -0,0 +1,18 @@
+package com.example.scrabblegueraultblanquet;
+
+import java.io.FileNotFoundException;
+
+public class Dictionary {
+
+    String[] wordList;
+
+    public Dictionary() throws FileNotFoundException
+    {
+
+    }
+
+    public boolean isValidWord(String word)
+    {
+        return true;
+    }
+}
