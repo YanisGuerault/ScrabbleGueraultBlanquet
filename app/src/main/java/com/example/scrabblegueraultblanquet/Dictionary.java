4770cc1019905b884070c19e11f5b87a4704d54b
Yanis Guérault <yanis.guerault@orange.com>
2019-12-13 12:22:28 +0100

    Etape 2

--- app/src/main/java/com/example/scrabblegueraultblanquet/Dictionary.java
+++ app/src/main/java/com/example/scrabblegueraultblanquet/Dictionary.java
@@ -1,18 +1,93 @@
 package com.example.scrabblegueraultblanquet;
 
-import java.io.FileNotFoundException;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.util.Log;
+import androidx.core.content.ContextCompat;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.text.Normalizer;
+import java.util.LinkedList;
+import java.util.List;
+
+import static java.util.Arrays.binarySearch;
+import static java.util.Arrays.sort;
 
 public class Dictionary {
 
-    String[] wordList;
+    public String[] wordList;
 
-    public Dictionary() throws FileNotFoundException
-    {
+    public Dictionary(Context C) {
 
+        InputStream ips = C.getResources().openRawResource(R.raw.frutf8);
+        InputStreamReader ipsr = new InputStreamReader(ips);
+        BufferedReader br = new BufferedReader(ipsr);
+        String line;
+        try {
+            line = br.readLine();
+            Log.i("Scrabble", line);
+            wordList = new String[Integer.parseInt(line)];
+            int i = 0;
+            while ((line = br.readLine()) != null) {
+                Log.i("Scrabble",String.valueOf(i));
+                wordList[i] = line;
+                i++;
+            }
+        } catch (Exception e) {
+            Log.e("Scrabble","Error : "+e.getMessage());
+        }
     }
 
-    public boolean isValidWord(String word)
-    {
-        return true;
+    public boolean isValidWord(String word) {
+        sort(wordList);
+        return binarySearch(wordList, word) >= 0;
+    }
+
+    public static boolean mayBeComposed(String word, char[] letters) {
+        LinkedList<Character> list = new LinkedList<Character>();
+        LinkedList<Character> tabChar = new LinkedList<Character>();
+
+        word = replaceFrenchCharacter(word);
+        letters = replaceFrenchCharacter(String.valueOf(letters)).toCharArray();
+
+        for (int i = 0; i < word.length(); i++) {
+            tabChar.add(word.charAt(i));
+        }
+
+        for (char c : letters) {
+            list.add(c);
+        }
+
+        for (char ch : list) {
+            if (tabChar.contains(ch)) {
+                tabChar.remove(Character.valueOf(ch));
+            }
+        }
+
+
+        if (tabChar.size() > 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public List<String> getWordsThatCanBeComposed(char[] letters) {
+        LinkedList<String> result = new LinkedList<>();
+        for (String word : wordList) {
+            if (mayBeComposed(word, letters))
+                result.add(word);
+        }
+
+        return result;
+    }
+
+    public static String replaceFrenchCharacter(String s) {
+        s = s.toLowerCase();
+        s = Normalizer.normalize(s,Normalizer.Form.NFD);
+        return s.replaceAll("[\\p{InCOMBINING_DIACRITICAL_MARKS}]","");
     }
 }
--- app/src/main/java/com/example/scrabblegueraultblanquet/MainActivity.java
+++ app/src/main/java/com/example/scrabblegueraultblanquet/MainActivity.java
@@ -1,13 +1,58 @@
 package com.example.scrabblegueraultblanquet;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.util.Log;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.core.content.ContextCompat;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[] {Manifest.permission.READ_SMS}, 1);
+        }
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Dictionary dic = new Dictionary(getApplicationContext());
+                char[] ch = {'b', 'j', 'o', 'o', 'n', 'r', 'ü'};
+                List<String> word = dic.getWordsThatCanBeComposed(ch);
+                Log.i("Scrabble", String.valueOf(word));
+            };
+        });
+        thread.run();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        switch(requestCode)
+        {
+            case 1:
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    Toast toast = Toast.makeText(getApplicationContext(),"Permission ReadSMS acceptée !",Toast.LENGTH_LONG);
+                    toast.show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+                break;
+        }
     }
 }
